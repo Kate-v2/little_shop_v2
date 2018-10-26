82775739ca20a2ae1baa14bb3cba425a8fb2b19b
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-25 21:20:40 -0600

    reorder routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,24 @@
 Rails.application.routes.draw do
 
+  root "welcome#index"
+
+  get '/login',  to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  get '/profile',   to: 'users#show'
+  get '/merchants', to: 'user#index'
 
   # get '/dashboard', to: 'users#index'
   resources :items
+  
   resources :users, only: [:new, :create] do
     resources :items, only: [:new, :create]
   end
 
 
-  root "welcome#index"
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
-  get '/profile', to: 'users#show'
-  get '/merchants', to: 'user#index'
+
+
+
 
 end
