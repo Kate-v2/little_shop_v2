8f0b45e9e765dbb5abcf17ca8a190ac7ce3c95f5
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-25 22:45:59 -0600

    User Story 1 routes created

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,22 @@ Rails.application.routes.draw do
 
   root "welcome#index"
 
-  get '/register', to: 'users#new'
+  get '/register',  to: 'users#new'
   post '/register', to: 'users#create'
   get '/login',  to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
+  get '/cart', to: 'cart#index'
+
   get '/profile',   to: 'users#show'
-  get '/merchants', to: 'user#index'
+  get '/dashboard', to: 'dashboard#index'
+
+  # something not quite right with this path
+  get '/merchants', to: 'users#index'
 
   # get '/dashboard', to: 'users#index'
-  resources :items
+  resources :items, only: [:index]
 
   resources :users, only: [:new, :create] do
     resources :items, only: [:new, :create]
