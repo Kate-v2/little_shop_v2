f45f3bb55752d39a535810670cf50740cd0f2287
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-25 22:00:38 -0600

    html logic & some new resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root "welcome#index"
 
+  get '/register', to: 'users#new'
+  post '/register', to: 'users#create'
   get '/login',  to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
@@ -11,7 +13,7 @@ Rails.application.routes.draw do
 
   # get '/dashboard', to: 'users#index'
   resources :items
-  
+
   resources :users, only: [:new, :create] do
     resources :items, only: [:new, :create]
   end
