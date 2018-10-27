fe1ac770ea4044f187b9fbf78fc4fe24c318d10f
Meghan Stang <mstang15@gmail.com>
2018-10-27 15:43:38 -0600

    adds flash messages when +- to cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -11,7 +11,15 @@ class CartController < ApplicationController
   end
 
   def update
+    item = Item.find(params[:item_id])
+    new_qty = params[:number].to_i
+    original_qty = session[:cart][params[:item_id]].to_i
     session[:cart][params[:item_id]] = params[:number].to_i
+    if original_qty - new_qty == 1
+      flash[:success] = "#{item.name.capitalize} removed from cart"
+    else
+      flash[:success] = "#{item.name.capitalize} added to cart"
+    end
     redirect_back(fallback_location: root_path)
   end
 
