868fae0039a01ffd87a703dc31a8a6fdbb12ffaf
Quinn Krug <quinn.krug@gmail.com>
2018-10-28 21:03:35 -0600

    only admin has access to user index

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,15 @@
 class Admin::UsersController <ApplicationController
 
+  before_action :require_admin
+
   def index
-    @users = User.all 
+    @users = User.all
+  end
+
+  private
+
+  def require_admin
+    render file: "public/404" unless current_admin?
   end
 
 end
