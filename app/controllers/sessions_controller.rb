0a7ee2722b90c8d1f23e44ce8022b10ceb8da8b7
Meghan Stang <mstang15@gmail.com>
2018-10-29 21:25:30 -0600

    fixes active bug in session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,13 +10,15 @@ class SessionsController <ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    if user.active && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      flash[:login] = "Welcome, #{user.name}!"
-      redirect_to profile_path
-    elsif !user.active
-      flash[:error]= "Your account has been disabled. Please contact your administrator for details."
-      render :new
+    if user && user.authenticate(params[:password])
+      if user.active
+        session[:user_id] = user.id
+        flash[:login] = "Welcome, #{user.name}!"
+        redirect_to profile_path
+      else
+        flash[:error]= "Your account has been disabled. Please contact your administrator for details."
+        render :new
+      end
     else
       flash[:login] = "Incorrect email/password combination."
       render :new
