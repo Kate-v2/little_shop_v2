0d382fecd270129c4e30f778e0522673f2bd3d39
Meghan Stang <mstang15@gmail.com>
2018-10-29 20:48:23 -0600

    adds inability to log in if user is disabled

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,13 +10,13 @@ class SessionsController <ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    # binding.pry
-    if user && user.authenticate(params[:password])
-      # binding.pry
+    if user.active && user.authenticate(params[:password])
       session[:user_id] = user.id
-      # binding.pry
       flash[:login] = "Welcome, #{user.name}!"
       redirect_to profile_path
+    elsif !user.active
+      flash[:error]= "Your account has been disabled. Please contact your administrator for details."
+      render :new
     else
       flash[:login] = "Incorrect email/password combination."
       render :new
