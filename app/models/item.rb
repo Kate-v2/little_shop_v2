f135e404f4aec3e50b802657915940171ae97afd
Quinn Krug <quinn.krug@gmail.com>
2018-10-31 11:28:52 -0600

    updated item model to validate price and inv > 0

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ApplicationRecord
   validates_presence_of :name,
-                        :price,
                         :description,
-                        :inventory,
                         :user_id
+  validates :price, presence: true, :numericality => { greater_than: 0}
+  validates :inventory, presence: true, :numericality => { greater_than_or_equal_to: 0}
 
   has_many :order_items
   has_many :orders, through: :order_items
