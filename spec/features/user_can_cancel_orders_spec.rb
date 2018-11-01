f110f94a30c52ea4e69d4ef4e41f9a3b6eb5cec0
Katelyn <katelyn.rimkunas@gmail.com>
2018-11-01 07:59:27 -0600

    user can cancel their purchases

--- spec/features/user_can_cancel_orders_spec.rb
+++ spec/features/user_can_cancel_orders_spec.rb
@@ -6,37 +6,61 @@ describe 'Any User can CANCEL a relevant order' do
 
   before(:each) do
     # -- shops --
-    # @merch1 = create(:user, role: 1)
-    # create_list(:item, 1, user_id: @merch1.id)
-    #
-    # @merch2 = create(:user, role: 1)
-    # create_list(:item, 1, user_id: @merch2.id)
-    #
-    # # -- purchasers --
-    # @user1 = create(:user, role: 0)
-    # login(@user1)
-    # shop; checkout
-    # # order has item 1 from merch 1
-    # # order has item 2 from merch 2
-    #
-    # @user2 = create(:user, role: 0)
-    # login(@user2)
-    # shop; checkout
-    # # order has item 1 from merch 1
-    # # order has item 2 from merch 2
-    #
+    @merch1 = create(:user, role: 1)
+    create_list(:item, 1, user_id: @merch1.id)
+
+    @merch2 = create(:user, role: 1)
+    create_list(:item, 1, user_id: @merch2.id)
+
+    # -- purchasers --
+    @user1 = create(:user, role: 0)
+    login(@user1)
+    shop; checkout
+    # order has item 1 from merch 1
+    # order has item 2 from merch 2
+    mock_order([Item.first], 1, @user1)
+    # order has item 1 from merch 1
+
+    @user2 = create(:user, role: 0)
+    login(@user2)
+    shop; checkout
+    # order has item 1 from merch 1
+    # order has item 2 from merch 2
+
     # item = Item.where(user_id: @merch3.id)
     # @excluded_order = mock_order(item, 2, @user1 )
-    #
-    # # --- Merchant Experience ---
-    # login(@merch1)
-    # visit dashboard_orders_path
   end
 
   describe 'User' do
 
-    it "can cancel their own purchased orders" do
-      skip
+    it "can cancel their own purchased orders from the index page" do
+      login(@user1)
+      visit profile_orders_path
+
+      order = Order.first
+      order1 = page.find("#order-#{order.id}")
+
+      # -- from index --
+      expect(page).to_not have_content("Status: Canceled")
+      order1.click_on("Cancel Order")
+      expect(page).to have_current_path(profile_orders_path)
+      expect(page).to have_content("Status: Canceled")
+    end
+
+    it "can cancel their own purchased orders from the show page" do
+      login(@user1)
+      visit profile_orders_path
+
+      order = Order.first
+      order1 = page.find("#order-#{order.id}")
+
+      # -- from show --
+      order1.click_on("Order: #{order.id}")
+      expect(page).to have_current_path(profile_order_path(order))
+      expect(page).to have_content("Status: Pending")
+      click_on("Cancel Order")
+      expect(page).to have_current_path(profile_order_path(order))
+      expect(page).to have_content("Status: Canceled")
     end
 
     it "cannot cancel other user orders" do
